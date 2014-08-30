f6cd407bc621cdb11cdf537d713b75199120703b
Florida Elago <florida.elago@gmail.com>
2014-08-30 14:21:52 -0700

    dont need that var

--- transformer.js
+++ transformer.js
@@ -2,7 +2,6 @@ module.exports = function (imageBuffer, callback) {
   var fs = require('fs'),
       gm = require('gm');
 
-  var modBuffer = ""
   gm(imageBuffer)
   .flip()
   .toBuffer(function(err, buffer) {
